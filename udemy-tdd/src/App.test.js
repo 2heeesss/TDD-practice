5335646a2fa324598f00837cc0df78ab12dafc1e
2heeesss <hohooodo@gmail.com>
2022-02-20 01:00:33 +0900

    [Test] Routing test

--- udemy-tdd/src/App.test.js
+++ udemy-tdd/src/App.test.js
@@ -1,8 +1,58 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Routing', () => {
+  const setup = path => {
+    window.history.pushState({}, '', path);
+    render(<App />);
+  };
+  it.each`
+    path         | pageTestId
+    ${'/'}       | ${'home-page'}
+    ${'/signup'} | ${'signup-page'}
+    ${'/login'}  | ${'login-page'}
+    ${'/user/1'} | ${'user-page'}
+    ${'/user/2'} | ${'user-page'}
+  `('display $pageTestId when path is $path', ({ path, pageTestId }) => {
+    window.history.pushState({}, '', path);
+    render(<App />);
+    const page = screen.queryByTestId(pageTestId);
+    expect(page).toBeInTheDocument();
+  });
+
+  it.each`
+    path         | pageTestId
+    ${'/'}       | ${'signup-page'}
+    ${'/signup'} | ${'home-page'}
+    ${'/'}       | ${'user-page'}
+  `('does not display $pageTestId when path is $path', ({ path, pageTestId }) => {
+    window.history.pushState({}, '', path);
+    render(<App />);
+    const page = screen.queryByTestId(pageTestId);
+    expect(page).not.toBeInTheDocument();
+  });
+
+  it.each`
+    targetPage
+    ${'Home'}
+    ${'Sign Up'}
+    ${'Login'}
+  `('has link to $targetPage on NavBar', ({ targetPage }) => {
+    setup('/');
+    const link = screen.getByRole('link', { name: targetPage });
+    expect(link).toBeInTheDocument();
+  });
+
+  it.each`
+    initialPath  | clickingTo   | visiblePage
+    ${'/'}       | ${'Sign Up'} | ${'signup-page'}
+    ${'/signup'} | ${'Home'}    | ${'home-page'}
+    ${'/signup'} | ${'Login'}   | ${'login-page'}
+  `('has has Sign up text when click sign-up link', ({ initialPath, clickingTo, visiblePage }) => {
+    setup(initialPath);
+    const link = screen.getByRole('link', { name: clickingTo });
+    userEvent.click(link);
+    expect(screen.getByTestId(visiblePage)).toBeInTheDocument();
+  });
 });
