0152130680c0a3b9b219547e0f8e0fec959c5956
2heeesss <hohooodo@gmail.com>
2022-02-20 00:59:41 +0900

    [Feat] add Routing and navbar

--- udemy-tdd/src/App.jsx
+++ udemy-tdd/src/App.jsx
@@ -1,9 +1,38 @@
-import './App.css';
+import { useState } from 'react';
+
+import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
+import LoginPage from './pages/LoginPage';
 
 const App = function () {
+  const [path, setPath] = useState(window.location.pathname);
+
+  const onClickLink = event => {
+    event.preventDefault();
+    const path = event.target.attributes.href.value;
+    window.history.pushState({}, '', path);
+    setPath(path);
+  };
+
   return (
     <>
-      <div>haha</div>
+      <div>
+        <a href="/" title="Home" onClick={onClickLink}>
+          TDD
+        </a>
+        <a href="/signup" title="Sign Up" onClick={onClickLink}>
+          sign up
+        </a>
+        <a href="/login" title="Login" onClick={onClickLink}>
+          login
+        </a>
+      </div>
+
+      {window.location.pathname === '/' && <HomePage />}
+      {window.location.pathname === '/signup' && <SignUpPage />}
+      {window.location.pathname.startsWith('/user/') && <UserPage />}
+      {window.location.pathname === '/login' && <LoginPage />}
     </>
   );
 };
